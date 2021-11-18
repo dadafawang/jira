2a1ef7247a48f97d00421753330b195caf71f94a
wangya <wangya@kezaihui.com>
2021-11-18 17:15:00 +0800

    chore: jsx2tsx

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import * as qs from "qs";
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { cleanObject, useDebounce } from "utils";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [users, setUsers] = useState([]);
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+  const debouncedParam = useDebounce(param, 200);
+
+  useEffect(() => {
+    // TODO: 这里请求接口200，但preview里出现“You need to enable JavaScript to run this app.”
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List users={users} list={list} />
+    </div>
+  );
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,13 +1,37 @@
-export const isFalsy = (value: number) => (value === 0 ? false : !value);
+import { useEffect, useState } from "react";
+
+export const isFalsy = (value: unknown) => (value === 0 ? false : !value);
 
 // 在一个函数里，改变传入的对象本身是不好的
-export const cleanObject = (object: any) => {
+export const cleanObject = (object: object) => {
   const result = { ...object };
   Object.keys(result).forEach((key) => {
+    // @ts-ignore
     const value = result[key];
     if (isFalsy(value)) {
+      // @ts-ignore
       delete result[key];
     }
   });
   return result;
+};
+
+export const useMount = (callback: () => void) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+// TODO：之后用泛型来规范类型
+export const useDebounce = (value: unknown, delay?: number): any => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在value变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebouncedValue(value), delay);
+    // 每次在上一个useEffect处理完成以后再运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+
+  return debouncedValue;
 };
